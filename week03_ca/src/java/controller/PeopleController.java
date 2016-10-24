28607a3c23eb7c7e0492bfa46804a341ee42c92f
LAPTOP-KDB9RUK3\samru <samruddhi226@gmail.com>
2016-10-24 15:23:21 +0800

    AppConfig

---
+++ week03_ca/src/java/controller/PeopleController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.inject.Named;
+
+/**
+ *
+ * @author samru
+ */
+
+@Named("peopleCtrl")
+public class PeopleController {
+        
+        private String name;
+        private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public void add(){
+        
+    }
+    
+        
+        
+}
---
+++ week03_ca/src/java/rest/AppConfig.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author samru
+ */
+public class AppConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return super.getClasses(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
